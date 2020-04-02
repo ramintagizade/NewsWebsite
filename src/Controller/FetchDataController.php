428a1760eeac8b698171bd79249b5128f8af7731
Ramin Taghizada <ramintagizade@mail.ru>
2020-04-02 18:54:42 +0400

    update data controller

--- src/Controller/FetchDataController.php
+++ src/Controller/FetchDataController.php
@@ -20,8 +20,15 @@ class FetchDataController extends AbstractController {
      */
     public function fetch() : Response {
 
+        if (isDataLoading()) {
+            return new Response("InProgress", Response::HTTP_OK);  
+        }
+
         $next_page = 1;
         $httpClient = HttpClient::create();
+        $categories = ["general", "business", "entertainment","health","science","sports","technology"];
+        // loop through the categories array and fetch for each category 
+
         $response = $httpClient->request('GET', 'https://newsapi.org/v2/top-headlines?page='.$next_page.'&country=us&category=general&apiKey=d60756acb1ff49248d829c1635cca29e');
 
         $statusCode = $response->getStatusCode();
@@ -35,6 +42,9 @@ class FetchDataController extends AbstractController {
             $articles = $content->articles;
             $totalResults = $content->totalResults;
             $pages = ceil($totalResults/20);
+            
+            
+        
 
             for($i=2;$i<=$pages;$i++) {
                 $content = sendRequest($i);
@@ -49,6 +59,28 @@ class FetchDataController extends AbstractController {
             return new Response('Data not available', Response::HTTP_NOT_FOUND);
         }
     }
+
+    public function saveByCategory($articles , $category) {
+        $len = count($articles);
+        for($i=0;$i<$len;$i++) {
+            // then call the mysql db to store the data 
+        }
+    }
+
+    /**
+     * @Route("/checkLoadingStatus", name="loading")
+     */
+
+     public function checkLoadingStatus() {
+        // check the mysql table to load new status , because sending requests for new data
+        // would make the fetching procedure freeze or override
+        
+     }
+
+     public function isDataLoading() {
+         // load from database either true or false
+         return true;
+     }
 
     public function sendRequest($next_page) {
         $httpClient = HttpClient::create();

3fc9d482909a4590ac632174fa2eab6ec16b7fe2
Ramin Taghizada <ramintagizade@mail.ru>
2020-04-22 14:16:11 +0400

    update page js file

--- public/static/index.js
+++ public/static/index.js
@@ -1,15 +1,29 @@
 
 document.getElementById("next_page").addEventListener("click",function(){
-    var page = window.location.href.split("?page=")[1];
+    var page = window.location.href.split("page=")[1];
     if (page==undefined) {
         page = 0;
     }
-    window.location.href = "?page=" + (parseInt(page)+1);
+    var url = window.location.href;
+
+    if(url.indexOf("/search?q=")>0) {
+        var pageIndex = url.indexOf("page=");
+        if(pageIndex>0) {
+
+            window.location.href = window.location.href.substring(0,pageIndex-1) +  "&page=" + (parseInt(page)+1);
+        }
+        else {
+            window.location.href+= "&page=" + (parseInt(page)+1);
+        }
+    }
+    else {
+        window.location.href = "?page=" + (parseInt(page)+1);        
+    }
 })
 
 
 window.addEventListener("load", function(){
-    var page = window.location.href.split("?page=")[1];
+    var page = window.location.href.split("page=")[1];
     if (page==undefined) {
         page = 0;
     }
@@ -19,11 +33,26 @@ window.addEventListener("load", function(){
 });
 
 document.getElementById("previous_page").addEventListener("click",function(){
-    var page = window.location.href.split("?page=")[1];
+    var page = window.location.href.split("page=")[1];
     if (page==undefined) {
         page = 0;
     }
-    if(page>0)
-        window.location.href = "?page=" + (parseInt(page)-1);
+    if(page>0) {
+        var url = window.location.href;
+
+        if(url.indexOf("/search?q=")>0) {
+            var pageIndex = url.indexOf("page=");
+            if(pageIndex>0) {
+
+                window.location.href = window.location.href.substring(0,pageIndex-1) +  "&page=" + (parseInt(page)-1);
+            }
+            else {
+                window.location.href+= "&page=" + (parseInt(page)-1);
+            }
+        }
+        else {
+            window.location.href = "?page=" + (parseInt(page)-1);        
+        }
+    }
 })
 
